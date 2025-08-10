550598bb7b038dac9b4a3d5dd9f47541a720efa0
samarth-kudari <samarthkudari3@gmail.com>
2025-08-10 13:32:02 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+# Task 1: Perform Basic Mathematical Operation
+a=input('Enter the first number: ')
+a=float(a)
+b=input('Enter the second number: ')
+b=float(b)
+c=a+b
+print('addition:',c)
+c=a-b
+print('subtraction:',c)
+c=a*b
+print('multiplication:',c)
+c=a/b
+print('Division:',c)
+
+
---
+++ task2.py
@@ -0,0 +1,7 @@
+#Task 2: Create a personalized Greeting
+first_name=input('Enter your first name: ')
+first_name=(first_name)
+last_name=input('Enter your last name: ')
+last_name=(last_name)
+
+print('hello, ',first_name,last_name,'! Welcome to python world')
